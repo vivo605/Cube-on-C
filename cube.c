c1ebad3aed10644eeead5b324d1e7424309876ef
Mzlf <mimimisha2110@gmail.com>
2025-10-04 20:18:52 +0300

    match

--- cube.c
+++ cube.c
@@ -5,6 +5,21 @@ double A = 0.01;
 double B = 0.01;
 double C = 0.01;
 
+double ooz;
+
+int x;
+int y;
+int z;
+int xp;
+int yp;
+int idx;
+
+int cubeWidth = 20;
+int width = 60;
+int height = 44;
+int K1 = 30;
+int distanceFromCam = 60;
+
 double calculateX(double j, double k, double i) {
     return j * sin(A) * sin(B) * cos(C) - k * cos(A) * sin(B)
     * cos(C) + j * cos(A) * sin(C) + k * sin(A) * sin(C) + i * cos(B)
@@ -21,17 +36,24 @@ double calculateZ(double j, double k, double i) {
     return k * cos(A) * cos(B) - j * sin(A) * cos(B) + i * sin(B);
 }
 
-int main() {
-    double x = 1.0, y = 2.0, z = 3.0;
-    
-    for (size_t i = 0; i < 10; i++) {
-        printf("X: %f, Y: %f, Z: %f\n", 
-               calculateX(x, y, z), 
-               calculateY(x, y, z), 
-               calculateZ(x, y, z));
-        A += 0.05;
-        B += 0.05;
-        C += 0.05;
+double calculateForSurface(double cubeX, double cubeY, double cubeZ, char ch) {
+    int x = calculateX(cubeX, cubeY, cubeZ);
+    int y = calculateY(cubeX, cubeY, cubeZ);
+    int z = calculateZ(cubeX, cubeY, cubeZ) + distanceFromCam;
+
+    ooz = 1/z;
+    xp = floor(width/2 + K1 * ooz * x *2);
+    yp = floor(height/2 + K1 * ooz * y);
+    idx = xp + yp * width;
+
+    if (idx >= 0 && idx < width * height) {
     }
+}
+
+int main() {
+    for (;;){
+        
+    }
+
     return 0;
 }
